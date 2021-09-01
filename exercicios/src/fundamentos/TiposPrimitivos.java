625307b197b34a45fb1e1a526e1564cc66e20840
galemagalhaes <g.alemagalhaes@gmail.com>
2021-09-01 16:56:39 -0300

    primeiros exercicios

---
+++ exercicios/src/fundamentos/TiposPrimitivos.java
@@ -0,0 +1,40 @@
+package fundamentos;
+
+public class TiposPrimitivos 
+{
+	public static void main(String[] args) 
+	{
+		//informações do funcionário
+		
+		//tipos numéricos inteiros
+		byte anosDeEmpresa = 23;
+		short numeroDeVoos = 542;
+		int id = 56789;
+		long pontosAcumulados = 3_134_845_223L;
+		
+		//Tipos numérico reais
+		float salario = 11_445.44F;
+		double vendasAcumuladas = 2_991_797_130.01;
+		
+		//Tipo booleano
+		boolean estaDeFerias = false; //true
+		
+		//Tipo caractere
+		char status = 'A'; //Ativo
+		//só cabe 1 caractere ou '\u0010' que significa um número na tabela unicode
+		
+		//Dias de empresa
+		System.out.println(anosDeEmpresa * 345);
+		
+		//Número de viagens
+		System.out.println(numeroDeVoos / 2);
+		
+		//Pontos por real
+		System.out.println(pontosAcumulados / vendasAcumuladas);
+		
+		System.out.println(id + ": ganha -> " + salario);
+		System.out.println("Férias? " + estaDeFerias);
+		System.out.println("Status " + status);
+	}
+
+}
