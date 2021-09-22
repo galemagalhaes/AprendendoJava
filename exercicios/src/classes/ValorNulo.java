700572cd4db9ad1c130c298454d6e0b291aee6b0
galemagalhaes <g.alemagalhaes@gmail.com>
2021-09-21 21:33:57 -0300

    Valor nulo e desafio do módulo

---
+++ exercicios/src/classes/ValorNulo.java
@@ -0,0 +1,28 @@
+package classes;
+
+public class ValorNulo {
+	public static void main(String[] args) {
+		String s1 = "";
+		System.out.println(s1.concat("!!!!!!"));
+		
+		//Data d1 = null;  ----isso tudo dá erro de runtime ou compilação
+		//d1.mes = 3;
+		
+		//String s2 = null;
+		//System.out.println(s2.concat("?????"));
+		
+		Data d1 = Math.random() > 0.5 ? new Data() : null;
+		
+		if(d1 != null) {
+			d1.mes = 3;
+			System.out.println(d1.obterDataFormatada());
+		}
+		
+		String s2 = Math.random() > 0.5 ? "Opa" : null;
+		
+		if(s2 != null) {
+			System.out.println(s2.concat("?????"));
+		}
+		
+	}
+}
