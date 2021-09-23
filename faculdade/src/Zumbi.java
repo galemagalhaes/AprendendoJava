6702524e48fd75141c57df67eb0976f3cd6ff88f
galemagalhaes <g.alemagalhaes@gmail.com>
2021-09-23 19:49:43 -0300

    teste classe publica

---
+++ faculdade/src/Zumbi.java
@@ -0,0 +1,14 @@
+
+public class Zumbi {
+	public static void main(String[] args) {
+		
+		String estado = "mortoVivo";
+		String cor = "verde";
+
+		if (estado == "mortoVivo" && cor == "verde") {
+
+			System.out.println("É um zumbi!");
+		}
+
+	}
+}
